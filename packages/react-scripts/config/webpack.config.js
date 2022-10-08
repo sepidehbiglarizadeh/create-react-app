59ffc2867970a523855af21e0f126d6f6d3ebc10
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-10-08 15:18:54 +0330

    fix css module

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -517,9 +517,7 @@ module.exports = function (webpackEnv) {
                 sourceMap: isEnvProduction
                   ? shouldUseSourceMap
                   : isEnvDevelopment,
-                modules: {
-                  mode: 'icss',
-                },
+                modules: true,
               }),
               // Don't consider CSS imports dead code even if the
               // containing package claims to have no side effects.
